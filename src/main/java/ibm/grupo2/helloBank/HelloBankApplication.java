9f3a0d17a6c16c35e7fbac34260cf5307f78e8b4
phenriquecunha <phcs-12@hotmail.com>
2022-09-04 11:59:45 -0300

    Initial structure

---
+++ src/main/java/ibm/grupo2/helloBank/HelloBankApplication.java
@@ -0,0 +1,13 @@
+package ibm.grupo2.helloBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibm/grupo2/helloBank/HelloBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.grupo2.helloBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
