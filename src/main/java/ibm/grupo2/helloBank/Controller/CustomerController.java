796e222327ca932efc3233dab20cd9d8b54bcb33
Eduardosilva3 <eduardo-silva159@hotmail.com>
2022-09-19 15:15:26 -0300

    SNS1

--- src/main/java/ibm/grupo2/helloBank/Controller/CustomerController.java
+++ src/main/java/ibm/grupo2/helloBank/Controller/CustomerController.java
@@ -40,7 +40,7 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<CustomerDto> create(@RequestBody @Valid CustomerDto clientDto){
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(iClientService.create(convertDtoToEntity(clientDto)).getId()).toUri();
-        String email = "teste@teste.com";
+        String email = "teste@teste.com.br";
         AWSSNSController sns = new AWSSNSController();
         sns.addSubscriptionToSNSTopic(email);
         sns.subTextSNS(clientDto.getPhone());
